2c5ad649ca1164346f965e5563b6eac8e6431578
Thomas Ruecker <thomas@ruecker.fi>
2014-07-24 23:03:28 +0300

    some test code based on Net::MQTT

--- mqtt-notify.pl
+++ mqtt-notify.pl
@@ -7,12 +7,13 @@
 # Based on growl-net.pl script by Alex Mason, Jason Adams.
 
 use strict;
-use vars qw($VERSION %IRSSI $AppName $MQTTUser $MQTTPass $MQTTServ $MQTTClient $MQTTTopic $MQTTTLS $MQTTPort $MQTTKeepalive $MQTTQOS $testing $Connection $j);
+use vars qw($VERSION %IRSSI $AppName $MQTTUser $MQTTPass $MQTTServ $MQTTClient $MQTTTopic $MQTTTLS $MQTTPort $MQTTKeepalive $MQTTRetain $MQTTQoS $testing $Connection $Connection_status $debug $message $j);
 
 use Irssi;
 use Net::MQTT::Constants;
 use Net::MQTT::Message;
 use IO::Socket::INET;
+use Time::HiRes;
 use utf8;
  
 $VERSION = '0.1';
@@ -39,6 +40,8 @@ sub cmd_mqtt_notify {
   Irssi::print('%G>>%n mqtt_notify_pass : Set to the sending accounts jabber password.');
   Irssi::print('%G>>%n mqtt_notify_tls : Set to enable TLS connection to mqtt server.');
   Irssi::print('%G>>%n mqtt_notify_port : Set to the mqtt server port number.');
+  Irssi::print('%G>>%n mqtt_notify_qos : Set to the desired mqtt QoS level.');
+  Irssi::print('%G>>%n mqtt_notify_retain : Set to the desired retain flag value.');
 }
 
 sub cmd_mqtt_notify_test {
@@ -66,6 +69,8 @@ Irssi::settings_add_bool($IRSSI{'name'}, 'mqtt_notify_tls',       0);
 Irssi::settings_add_int($IRSSI{'name'},  'mqtt_notify_port',      1883);
 Irssi::settings_add_int($IRSSI{'name'},  'mqtt_notify_keepalive', 120);
 Irssi::settings_add_int($IRSSI{'name'},  'mqtt_notify_qos',       0);
+Irssi::settings_add_int($IRSSI{'name'},  'mqtt_notify_retain',    0);
+Irssi::settings_add_int($IRSSI{'name'},  'mqtt_notify_debug',     0);
 
 $MQTTUser      = Irssi::settings_get_str('mqtt_notify_user');
 $MQTTPass      = Irssi::settings_get_str('mqtt_notify_pass');
@@ -75,52 +80,94 @@ $MQTTClient    = Irssi::settings_get_str('mqtt_notify_client');
 $MQTTTLS       = Irssi::settings_get_bool('mqtt_notify_tls');
 $MQTTPort      = Irssi::settings_get_int('mqtt_notify_port');
 $MQTTKeepalive = Irssi::settings_get_int('mqtt_notify_keepalive');
-$MQTTQOS       = Irssi::settings_get_int('mqtt_notify_qos');
+$MQTTQoS       = Irssi::settings_get_int('mqtt_notify_qos');
+$MQTTRetain    = Irssi::settings_get_int('mqtt_notify_retain');
+$debug         = Irssi::settings_get_int('mqtt_notify_debug');
 $AppName       = "irssi $MQTTServ";
 
 #$Connection = Net::Jabber::Client->new();
+$Connection = IO::Socket::INET->new(PeerAddr => $MQTTServ.':'.$MQTTPort,
+                                    Timeout => $MQTTKeepalive,) or $Connection_status = "Connection failed: $!";
+#FIXME - handle failure and reconnect
 
-#my $status = $Connection->Connect(
-#  "hostname" => $XMPPServ,
-#  "port" => $XMPPPort,
-#  "componentname" => $XMPPDomain,
-#  "tls" => $XMPPTLS );
+my $Connection_buffer = '';
+my $next_ping;
+my $received_ping_ack = 1;
 
+if (defined($Connection))
+{
+  send_message($Connection, message_type => MQTT_CONNECT,
+                keep_alive_timer => $MQTTKeepalive,);
+  my $Connection_response = read_message($Connection, $Connection_buffer) or $Connection_status = "No ConnAck";
+  Irssi::print('Connection response: '.$Connection_response) if ($debug >=1);
+}
+#FIXME see if this is useful in the long run.
 
-
-if (!(defined($status)))
+if (defined($Connection_status))
 {
   Irssi::print("ERROR:  MQTT server is down or connection was not allowed.");
-  Irssi::print ("        ($!)");
+  Irssi::print("        ($Connection_status)");
   return;
 }
 
+#FIXME just for testing, replace this
+sub send_message {
+  my $Connection = shift;
+  my $messsage = Net::MQTT::Message->new(@_);
+  Irssi::print('Sending: '.$message->string)  if ($debug >= 1);
+  $message = $message->bytes;
+  syswrite $Connection, $message, length $message;
+  Irssi::print(dump_string($message, 'Sent: ')) if ($debug >= 2);
+  $next_ping = Time::HiRes::time + $MQTTKeepalive;
+}
 
-#my @result = $Connection->AuthSend(
-#  "username" => $XMPPUser,
-#  "password" => $XMPPPass,
-#  "resource" => $XMPPRes );
-
-
-
-#if ($result[0] ne "ok")
-#{
-#  Irssi::print("ERROR: Authorization failed ($XMPPUser".'@'."$XMPPDomain on server $XMPPServ) : $result[0] - $result[1]");
-#  return;
-#}
-#Irssi::print ("Logged into server $XMPPServ as $XMPPUser".'@'."$XMPPDomain. Sending notifications to $XMPPRecv.");
+#FIXME just for testing, replace this
+sub read_message {
+  my $Connection = shift;
+  my $select = IO::Select->new($Connection);
+  $select->add(\*STDIN) if (@_ > 1);
+  my $timeout = $next_ping - Time::HiRes::time;
+  do {
+    my $mqtt = Net::MQTT::Message->new_from_bytes($_[0], 1);
+    return $mqtt if (defined $mqtt);
+    return $1 if (@_ > 1 && $_[1] =~ s/^(.*?)\n//);
+    my @handles = $select->can_read($timeout) or return;
+    $timeout = $next_ping - Time::HiRes::time;
+    foreach my $handle (@handles) {
+      if ($handle eq $Connection) {
+        my $bytes = sysread $Connection, $_[0], 2048, length $_[0];
+        unless ($bytes) {
+          die "Socket closed ", (defined $bytes ? 'gracefully' : 'error'), "\n";
+        }
+        print "Receive buffer: ", dump_string($_[0], '   '), "\n\n"
+          if ($debug >= 2);
+      } else {
+        my $bytes = sysread STDIN, $_[1], 2048, length $_[1];
+        exit unless ($bytes);
+      }
+    }
+  } while ($timeout > 0);
+  return;
+}
+ 
+ 
 
 sub sig_message_private ($$$$) {
   return unless Irssi::settings_get_bool('mqtt_show_privmsg');
 
   my ($server, $data, $nick, $address) = @_;
 
-#  my $message = new Net::Jabber::Message();
   my $body = '(Private message from: '.$nick.')';
   if ((Irssi::settings_get_bool('mqtt_reveal_privmsg'))) {
     $body = '(PM: '.$nick.') '.$data;
   }
   utf8::decode($body);
+  send_message($Connection,
+               message_type => MQTT_PUBLISH,
+               retain => $MQTTRetain,
+               qos => $MQTTQoS,
+               topic => $MQTTTopic,
+               message => $body,);
 #  $message->SetMessage(to=>$XMPPRecv);
 #  $message->SetMessage(
 #    type=>"chat",
@@ -201,4 +248,3 @@ Irssi::signal_add_last('message topic', \&sig_message_topic);
 
 
 Irssi::print('%G>>%n '.$IRSSI{name}.' '.$VERSION.' loaded (/mqtt-notify for help. /mqtt-test to test.)');
-
