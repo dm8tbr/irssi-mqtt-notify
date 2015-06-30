4b5144bce98d6e3f8ca97787b392c0fd079489ed
Thomas B. Ruecker <thomas@ruecker.fi>
2015-06-30 05:59:06 +0000

    Clarify license, bump version to 1.0
    * BSD 3 clause, added SPDX string and full license text.
    * Bumped to 1.0 as it's been working long enough.

--- README.md
+++ README.md
@@ -2,6 +2,9 @@ irssi-MQTT-notify
 =================
 
 My MQTT notification script, based on my Jabber/XMPP notification script:
-https://github.com/dm8tbr/irssi-jabber-notify
+https://github.com/dm8tbr/irssi-xmpp-notify
+
+This script requires the Mosquitto client tools, specifically 'mosquitto_pub',
+to work.
 
 See https://github.com/dm8tbr/irssi-mqtt-sailfish for client side implementations.
--- mqtt-notify.pl
+++ mqtt-notify.pl
@@ -5,6 +5,39 @@
 # highlighted, and when you receive private messages.
 # Based on jabber-notify.pl script by Peter Krenesky, Thomas Ruecker.
 # Based on growl-net.pl script by Alex Mason, Jason Adams.
+#
+# You can find the script on GitHub: https://github.com/dm8tbr/irssi-mqtt-notify/
+# Please report bugs to https://github.com/dm8tbr/irssi-mqtt-notify/issues
+#
+# Copyright (c) 2015, Thomas B. Ruecker
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from this
+# software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
 
 use strict;
 use vars qw($VERSION %IRSSI $AppName $MQTTUser $MQTTPass $MQTTServ $MQTTClient $MQTTTopic $MQTTTLS $MQTTPort $MQTTKeepalive $MQTTRetain $MQTTQoS @args  $j);
@@ -13,13 +46,13 @@ use Irssi;
 use utf8;
 use POSIX;
  
-$VERSION = '0.1';
+$VERSION = '1.0';
 %IRSSI = (
   authors      =>   'Thomas B. Ruecker',
   contact      =>   'thomas@ruecker.fi, tbr on irc.freenode.net',
   name         =>   'MQTT-notify',
-  description  =>   'Sends out notifications via MQTT for Irssi',
-  license      =>   'BSD',
+  description  =>   'Sends out notifications via MQTT',
+  license      =>   'BSD-3-Clause',
   url          =>   'http://github.com/dm8tbr/irssi-mqtt-notify/',
 
 );
